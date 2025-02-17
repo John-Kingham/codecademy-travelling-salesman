5f13dfbb0ff2d18eaab331ddd472c60fa81805ea
John Kingham <196238029+John-Kingham@users.noreply.github.com>
2025-02-17 14:09:59 +0000

    Added Graph class and tests and improved some docstrings.

--- tests/travelling_salesperson_test.py
+++ tests/travelling_salesperson_test.py
@@ -18,8 +18,52 @@ class TestVertex(unittest.TestCase):
 
 class TestGraph(unittest.TestCase):
 
-    @unittest.skip("skip due to partial implementation")
-    def test_has_path(self):
+    def test_has_path_to_direct_neighbour(self):
         graph = Graph()
-        graph.add_vertex(self.vertex)
-        self.fail()
+        vertex_a = Vertex("A")
+        vertex_b = Vertex("B")
+        vertex_c = Vertex("C")
+        graph.add_vertex(vertex_a)
+        graph.add_vertex(vertex_b)
+        graph.add_vertex(vertex_c)
+        graph.add_edge(vertex_a, vertex_b)
+        graph.add_edge(vertex_b, vertex_c)
+        self.assertTrue(graph.has_path(vertex_a.value, vertex_b.value))
+
+    def test_has_path_to_indirect_vertex(self):
+        graph = Graph()
+        vertex_a = Vertex("A")
+        vertex_b = Vertex("B")
+        vertex_c = Vertex("C")
+        graph.add_vertex(vertex_a)
+        graph.add_vertex(vertex_b)
+        graph.add_vertex(vertex_c)
+        graph.add_edge(vertex_a, vertex_b)
+        graph.add_edge(vertex_b, vertex_c)
+        self.assertTrue(graph.has_path(vertex_a.value, vertex_c.value))
+
+    def test_no_path(self):
+        graph = Graph()
+        vertex_a = Vertex("A")
+        vertex_b = Vertex("B")
+        graph.add_vertex(vertex_a)
+        graph.add_vertex(vertex_b)
+        self.assertFalse(graph.has_path(vertex_a.value, vertex_b.value))
+
+    def test_undirected_graph_makes_bidirectional_edges(self):
+        graph = Graph()
+        vertex_a = Vertex("A")
+        vertex_b = Vertex("B")
+        graph.add_vertex(vertex_a)
+        graph.add_vertex(vertex_b)
+        graph.add_edge(vertex_a, vertex_b)
+        self.assertTrue(graph.has_path(vertex_b.value, vertex_a.value))
+
+    def test_directed_graph_makes_unidirectional_edges(self):
+        graph = Graph(True)
+        vertex_a = Vertex("A")
+        vertex_b = Vertex("B")
+        graph.add_vertex(vertex_a)
+        graph.add_vertex(vertex_b)
+        graph.add_edge(vertex_a, vertex_b)
+        self.assertFalse(graph.has_path(vertex_b.value, vertex_a.value))
--- travelling_salesperson.py
+++ travelling_salesperson.py
@@ -1,13 +1,34 @@
 class Vertex:
+    """
+    A vertex for use in graphs.
+    """
 
     def __init__(self, value):
         self.value = value
         self.edges = {}  # {vertex.value : weight}
 
-    def add_edge(self, vertex_value, weight):
-        self.edges[vertex_value] = weight
+    def add_edge(self, to_vertex_value, weight=0):
+        """
+        Adds an edge from this vertex to another.
+
+        Parameters
+        ----------
+        to_vertex_value : str
+            The Vertex.value of the neighbouring Vertex.
+        weight : int
+            The edge's weight.
+        """
+        self.edges[to_vertex_value] = weight
 
     def get_edges(self):
+        """
+        Returns Vertex.value for each neighbouring Vertex.
+
+        Returns
+        -------
+        list : str
+            Contains the Vertex.value of each neighbouring Vertex.
+        """
         return list(self.edges.keys())
 
     def get_edge_weight(self, vertex_value):
@@ -18,8 +39,58 @@ class Graph:
 
     def __init__(self, directed=False):
         self.directed = directed
-        self.adjacency_list = {}  # {vertex.value : vertex object}
+        self.vertices = {}  # {vertex.value : vertex object}
 
     def add_vertex(self, vertex):
-        pass
-        # TODO finish this off
+        """
+        Adds a vertex to the graph.
+
+        Parameters
+        ----------
+        vertex : Vertex
+        """
+        self.vertices[vertex.value] = vertex
+
+    def add_edge(self, from_vertex, to_vertex, weight=0):
+        """
+        Adds an edge between two vertices.
+        If the graph isn't directed, an edge is added in both directions.
+
+        Parameters
+        ----------
+        from_vertex : Vertex
+        to_vertex : Vertex
+        weight : int
+        """
+        self.vertices[from_vertex.value].add_edge(to_vertex.value, weight)
+        if not self.directed:
+            self.vertices[to_vertex.value].add_edge(from_vertex.value, weight)
+
+    def has_path(self, from_vertex_value, to_vertex_value):
+        """
+        Returns whether the graph has a path between two vertices.
+
+        Parameters
+        ----------
+        from_vertex_value : str
+            Vertex.value for the from vertex
+        to_vertex_value : str
+            Vertex.value for the to vertex
+
+        Returns
+        -------
+        bool
+            True if a path exists between from and to, False if not.
+        """
+        vertices_to_visit = [from_vertex_value]
+        seen = []
+        while vertices_to_visit:
+            vertex_value = vertices_to_visit.pop(0)
+            seen.append(vertex_value)
+            if vertex_value == to_vertex_value:
+                return True
+            neighbour_values = set(self.vertices[vertex_value].get_edges())
+            for neighbour_value in neighbour_values:
+                if neighbour_value not in seen:
+                    vertices_to_visit.append(neighbour_value)
+        return False
